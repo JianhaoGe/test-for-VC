4fd799a01814d1f706aab2dff1706c363b66f8f5
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-25 14:53:27 +0800

    Update MakeLinkContraints.m

--- Plan1/MakeLinkContraints.m
+++ Plan1/MakeLinkContraints.m
@@ -1,6 +1,6 @@
-function [L1,L1min,L1max]=MakeLinkContraints(arf3,Arrival5)
+function [L1,L1min,L1max]=MakeLinkContraints(arf3,Arrival2)
 M=10^8;
-s=size(Arrival5);
+s=size(Arrival2);
 num_train=s(1,1);
 L1=intvar(num_train,num_train,'full');
 L1min=-M*(1-arf3);
@@ -8,6 +8,6 @@ L1max=M*(1-arf3);
 
 for j=1:num_train
     for i=1:num_train
-        L1(i,j)=Arrival5(i,end)-Arrival5(j,1);
+        L1(i,j)=Arrival2(i,end)-Arrival2(j,1);
     end
 end
