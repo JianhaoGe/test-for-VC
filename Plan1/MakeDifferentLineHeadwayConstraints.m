9f344aedbc4bf6cfa725c5e5e1574b397c994629
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-25 16:16:50 +0800

    Update MakeDifferentLineHeadwayConstraints.m

--- Plan1/MakeDifferentLineHeadwayConstraints.m
+++ Plan1/MakeDifferentLineHeadwayConstraints.m
@@ -19,4 +19,6 @@ for i=1:length(CommonStopSet2)
      ConHeadway12(:,count)=Arrival2(:,id2)-Arrival1(:,id1);
      count=count+1;
      ConHeadway12(2:num_train,count)=Arrival1(2:num_train,id1)-Arrival2(1:end-1,id2);
+     %% ConHeadway12 is used to show the headway between the two successive trains of different lines at stations in the collinear corridor.
+     %% For trains of Line 1, the odd row of ConHeadway12 represents the headway with its front train and the even row represents the headway with its rear train.  
 end
