aa00fa29c371bf674db9c94b3715603e49fd4372
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-25 14:37:01 +0800

    Update MakeDifferentLineHeadwayConstraints.m

--- Plan1/MakeDifferentLineHeadwayConstraints.m
+++ Plan1/MakeDifferentLineHeadwayConstraints.m
@@ -1,19 +1,21 @@
-function [Headway15MAX,ConHeadway15,Headway15MIN,VCor15]=MakeDifferentLineHeadwayConstraints(Line1,Arrival1,Departure1,Line5,Arrival5,Departure5,MinH,MaxH,CommonStopSet2)
+function [Headway12MAX,ConHeadway12,Headway12MIN,VCor12]=MakeDifferentLineHeadwayConstraints(Line1,Arrival1,Departure1,Line2,Arrival2,Departure2,MinH,MaxH,CommonStopSet2)
 s=size(Arrival1);
 num_train=s(1,1);
-ConHeadway15=sdpvar(num_train,length(CommonStopSet2)*2);
-%Virtual coupling decision variables for the trains of Line 3, where the first row represents whether it is coupled with the front train and the second row represents whether it is coupled with the rear train
-VCor15=binvar(num_train,2);
-Headway15MIN=(MinH+40)*ones(num_train,length(CommonStopSet2)*2);
-Headway15MAX=(MaxH+40)*ones(num_train,length(CommonStopSet2)*2);
+ConHeadway12=sdpvar(num_train,length(CommonStopSet2)*2);
+
+%% VCor12 is the decision variable showing the virtual coupling relationship between trains of Line 1 and Line 2.
+%% The first row represents whether it is coupled with the front train and the second row represents whether it is coupled with the rear train.
+VCor12=binvar(num_train,2);
+Headway12MIN=MinH*ones(num_train,length(CommonStopSet2)*2);
+Headway12MAX=MaxH*ones(num_train,length(CommonStopSet2)*2);
 count=0;
 for i=1:length(CommonStopSet2)
-     line1=Line1(Line1.From==CommonStopSet2(i),:);
-     line5=Line5(Line5.From==CommonStopSet2(i),:);
-     id1=line1.ID;
-     id5=line5.ID;
+     line1=Line1(Line1==CommonStopSet2(i),:);
+     line2=Line2(Line2==CommonStopSet2(i),:);
+     id1=find(Line1==line1);
+     id2=find(Line2==line2);
      count=count+1;
-     ConHeadway15(:,count)=Arrival5(:,id5)-Arrival1(:,id1);
+     ConHeadway12(:,count)=Arrival2(:,id2)-Arrival1(:,id1);
      count=count+1;
-     ConHeadway15(2:num_train,count)=Arrival1(2:num_train,id1)-Arrival5(1:end-1,id5);
+     ConHeadway12(2:num_train,count)=Arrival1(2:num_train,id1)-Arrival2(1:end-1,id2);
 end
