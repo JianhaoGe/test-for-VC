dfb60d6a75e45d1b8f0641d3d3e92573a9a7bf1d
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-25 14:39:25 +0800

    Update MakeDifferentLineHeadwayConstraints.m

--- Plan1/MakeDifferentLineHeadwayConstraints.m
+++ Plan1/MakeDifferentLineHeadwayConstraints.m
@@ -1,4 +1,4 @@
-function [Headway12MAX,ConHeadway12,Headway12MIN,VCor12]=MakeDifferentLineHeadwayConstraints(Line1,Arrival1,Departure1,Line2,Arrival2,Departure2,MinH,MaxH,CommonStopSet2)
+function [Headway12MAX,ConHeadway12,Headway12MIN,VCor12,VCnot12]=MakeDifferentLineHeadwayConstraints(Line1,Arrival1,Departure1,Line2,Arrival2,Departure2,MinH,MaxH,CommonStopSet2)
 s=size(Arrival1);
 num_train=s(1,1);
 ConHeadway12=sdpvar(num_train,length(CommonStopSet2)*2);
@@ -6,6 +6,7 @@ ConHeadway12=sdpvar(num_train,length(CommonStopSet2)*2);
 %% VCor12 is the decision variable showing the virtual coupling relationship between trains of Line 1 and Line 2.
 %% The first row represents whether it is coupled with the front train and the second row represents whether it is coupled with the rear train.
 VCor12=binvar(num_train,2);
+VCnot12=binvar(num_train,2);
 Headway12MIN=MinH*ones(num_train,length(CommonStopSet2)*2);
 Headway12MAX=MaxH*ones(num_train,length(CommonStopSet2)*2);
 count=0;
