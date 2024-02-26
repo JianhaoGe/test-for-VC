a287b379ea2c3dfaa9ad32c1de73c4cd3165c1ea
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-26 10:38:17 +0800

    Update AWT.m

--- Plan1/AWT.m
+++ Plan1/AWT.m
@@ -1,22 +1,22 @@
-%%%This code is used to calculate passengers' average waiting time. 
+%% This code is used to calculate passengers' average waiting time. 
 
 Waittime11=double(Waittime11);
 Waittime12=double(Waittime12);
-Waittime51=double(Waittime51);
-Waittime52=double(Waittime52);
+Waittime21=double(Waittime21);
+Waittime22=double(Waittime22);
 Waittime11(isnan(Waittime11))=0;
 Waittime12(isnan(Waittime12))=0;
-Waittime51(isnan(Waittime51))=0;
-Waittime52(isnan(Waittime52))=0;
+Waittime21(isnan(Waittime21))=0;
+Waittime22(isnan(Waittime22))=0;
 Strandedtime=0;
 for i=1:size(double(Qstranded_line1_left),1)-1
     for j=1:size(double(Qstranded_line1_left),2)
         Strandedtime=Strandedtime+double(Qstranded_line1_left(i,j))*(Departure1(i+1,j)-Departure1(i,j));
     end
 end
-for i=1:size(double(Qstranded_line5_left),1)-1
-    for j=1:size(double(Qstranded_line5_left),2)
-        Strandedtime=Strandedtime+double(Qstranded_line5_left(i,j))*(Departure5(i+1,j)-Departure5(i,j));
+for i=1:size(double(Qstranded_line2_left),1)-1
+    for j=1:size(double(Qstranded_line2_left),2)
+        Strandedtime=Strandedtime+double(Qstranded_line2_left(i,j))*(Departure2(i+1,j)-Departure2(i,j));
     end
 end
-AverageWaittime=(sum(sum(Waittime11))+sum(sum(Waittime12))+sum(sum(Waittime51))+sum(sum(Waittime52))+Strandedtime)/(sum(sum(double(Qboard_line1_left)))+sum(sum(double(Qboard_line5_left))));
+AverageWaittime=(sum(sum(Waittime11))+sum(sum(Waittime12))+sum(sum(Waittime21))+sum(sum(Waittime22))+Strandedtime)/(sum(sum(double(Qboard_line1_left)))+sum(sum(double(Qboard_line2_left))));
