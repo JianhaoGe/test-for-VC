cfdde3fac2f2e5f219ed042ddef6bd6721b7a0d4
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-26 10:21:42 +0800

    Update MakeAlight.m

--- Plan1/MakeAlight.m
+++ Plan1/MakeAlight.m
@@ -1,18 +1,19 @@
-function [Qalight_Line1_right,Qalight_Line5_right,ZZ3,ZZ3min,ZZ3max,ZZ4,ZZ4min,ZZ4max]=MakeAlight(Departure1,Qboard_line1_left,Rate1,Departure5,Qboard_line5_left,Rate5,arf3)
+function [Qalight_Line1_right,Qalight_Line2_right,ZZ3,ZZ3min,ZZ3max,ZZ4,ZZ4min,ZZ4max]=MakeAlight(Departure1,Qboard_line1_left,Rate1,Departure2,Qboard_line2_left,Rate2,arf3)
 s1=size(Departure1);
 totaltrain1=s1(1,1);
 totalstation1=s1(1,2);
-s5=size(Departure5);
-totaltrain5=s5(1,1);
-totalstation5=s5(1,2);
+s2=size(Departure2);
+totaltrain2=s2(1,1);
+totalstation2=s2(1,2);
 M=99999;
 
+%% Constraint (24,45-47)
 
 Qalight_Line1_right=sdpvar(totaltrain1,totalstation1);
-Qalight_Line5_right=sdpvar(totaltrain5,totalstation5);
+Qalight_Line2_right=sdpvar(totaltrain2,totalstation2);
   
 Qalight_Line1_right(:,1)=0;
-Qalight_Line5_right(:,1)=0;
+Qalight_Line2_right(:,1)=0;
 for k=2:totalstation1
     count=1;
     for v=1:k-1
@@ -25,31 +26,34 @@ for k=2:totalstation1
     end
 end
 
-for k=2:totalstation5
+for k=2:totalstation2
     count=1;
     for v=1:k-1
         if count==1
-            Qalight_Line5_right(:,k)=Qboard_line5_left(:,v)*Rate5(v,k-1);
+            Qalight_Line2_right(:,k)=Qboard_line2_left(:,v)*Rate2(v,k-1);
         else
-            Qalight_Line5_right(:,k)=Qalight_Line5_right(:,k)+Qboard_line5_left(:,v)*Rate5(v,k-1);
+            Qalight_Line2_right(:,k)=Qalight_Line2_right(:,k)+Qboard_line2_left(:,v)*Rate2(v,k-1);
         end
         count=count+1;
     end
 end
 
-ZZ3=sdpvar(totaltrain5,totaltrain5,totalstation5,'full');
-ZZ3min=-M*repmat(arf3,[1,1,totalstation5]);
-ZZ3max=M*repmat(arf3,[1,1,totalstation5]);
+%% PAM is used to determine whether there are passengers boarding on the front train (which is connected with this train) and alighting from this train.
+%% If arf(ii,i)==1; ZZ3(ii,i,v)=Qboard_line2_left(ii,v).
 
-ZZ4=sdpvar(totaltrain5,totaltrain5,totalstation5,'full');
-ZZ4min=-M*(1-repmat(arf3,[1,1,totalstation5]));
-ZZ4max=M*(1-repmat(arf3,[1,1,totalstation5]));
-for i=1:totaltrain5
-    for ii=1:totaltrain5
-        for k=2:totalstation5-2
-            for v=k+1:totalstation5-1
-                ZZ3(ii,i,v)=ZZ4(ii,i,v)+Qboard_line5_left(ii,v);
-                Qalight_Line5_right(i,k)=Qalight_Line5_right(i,k)+ZZ3(ii,i,v)*Rate5(v,k-1);
+ZZ3=sdpvar(totaltrain2,totaltrain2,totalstation2,'full');
+ZZ3min=-M*repmat(arf3,[1,1,totalstation2]);
+ZZ3max=M*repmat(arf3,[1,1,totalstation2]);
+
+ZZ4=sdpvar(totaltrain2,totaltrain2,totalstation2,'full');
+ZZ4min=-M*(1-repmat(arf3,[1,1,totalstation2]));
+ZZ4max=M*(1-repmat(arf3,[1,1,totalstation2]));
+for i=1:totaltrain2
+    for ii=1:totaltrain2
+        for k=2:totalstation2-2
+            for v=k+1:totalstation2-1
+                %% Note: code omitted.
+                Qalight_Line2_right(i,k)=Qalight_Line2_right(i,k)+ZZ3(ii,i,v)*Rate2(v,k-1);
             end
         end
     end
