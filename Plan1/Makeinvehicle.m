bc63f6bbbc3d9edd44141d198950ad017738728f
JianhaoGe <160686665+JianhaoGe@users.noreply.github.com>
2024-02-26 10:07:03 +0800

    Update Makeinvehicle.m

--- Plan1/Makeinvehicle.m
+++ Plan1/Makeinvehicle.m
@@ -3,6 +3,7 @@ s=size(Qboard_left);
 totaltrain=s(1,1);
 totalstation=s(1,2)+1;
 
+%% Constraints (26-27,29)
 Qinvehicle_right=sdpvar(totaltrain,totalstation);
 
 Qinvehicle_right(:,1)=zeros(totaltrain,1);
