115851c04bac8476d62e194f01088f280c1d11c1
JianhaoGe <jianhao_getj@163.com>
2024-03-03 11:07:13 +0800

    提交

--- Plan1/MakeRemainCapacityinVehicle.m
+++
@@ -1,3 +0,0 @@
-function Cremain_right = MakeRemainCapacityinVehicle(Cmax,Qinvehicle_left)
-%% Constraint 27
-Cremain_right=Cmax-Qinvehicle_left;
--- Plan1/UIC406ofVC.m
+++ Plan1/UIC406ofVC.m
@@ -3,14 +3,14 @@ load('result.mat');
 %% Arrival_1 and Arrival_2 represent the arrival times of trains after compression.
 %% Departure_1 and Departure_2 represent the departure times of trains after compression.
 Arrival_1=Arrival1;
-Arrival_2=Arrival5;
+Arrival_2=Arrival2;
 Departure_1=Departure1;
 Departure_2=Departure2;
 Compression=zeros(1,39);
 kk=zeros(1,39);
 
 %% The method is the same as the algorithm introduced in Fig.7. 
-%% The code cannot be run directly. Some codes are omitted. Please supplement with an understanding of the algorithm before running.
+%% Binary variable VCor12 represents whether the train of Line 1 is virtually coupled with its front train or its rear train.
 ii=0;
 for i=1:19
     ii=ii+1;
@@ -76,7 +76,34 @@ for i=1:19
         end     
     end
     ii=ii+1;
-    %% Note: code omitted.
+    if VCor12(i+1,2)==0
+        Compression(ii)=min([Departure_1(i+1,16:24)-Departure_2(i,18:26)-30,Departure_1(i+1,1:end-1)-Departure_1(i,1:end-1)-120]);
+        if Compression(ii)<0
+            disp('Warning!');
+        else
+            if Compression(ii)>0
+                Departure_1(i+1,:)=Departure_1(i+1,:)-Compression(ii);
+                Arrival_1(i+1,:)=Arrival_1(i+1,:)-Compression(ii);
+            end
+        end
+        if min(Departure_1(i+1,16:24)-Departure_2(i,18:26))-min(Departure1(i+1,16:24)-Departure2(i,18:26))~=0
+            kk(ii)=min(Departure_1(i+1,16:24)-Departure_2(i,18:26))-min(Departure1(i+1,16:24)-Departure2(i,18:26));
+            Departure_2(i,:)=Departure_2(i,:)+kk(ii);
+            Arrival_2(i,:)=Arrival_2(i,:)+kk(ii);
+        end
+    else
+        if VCor12(i+1,2)==1
+            Compression(ii)=min([Departure_1(i+1,16:24)-Departure_2(i,18:26)-120,Departure_1(i+1,1:end-1)-Departure_1(i,1:end-1)-120]);
+        if Compression(ii)<0
+            disp('Warning!');
+        else
+            if Compression(ii)>0
+                Departure_1(i+1,:)=Departure_1(i+1,:)-Compression(ii);
+                Arrival_1(i+1,:)=Arrival_1(i+1,:)-Compression(ii);
+            end
+        end
+        end
+    end
 end
 i=i+1;
 ii=ii+1;
